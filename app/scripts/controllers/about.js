fafe90770f5ac50f074c3947d5e198c8825afb2d
lekova <lina.lekova@gmail.com>
2015-10-08 22:00:30 -0400

    Initial commit
    App created using yeoman scaffolding.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gameApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gameApp
+ */
+angular.module('gameApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gameApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gameApp
+ */
+angular.module('gameApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
