9bcc8c1fa740f068a2789a1fed93ee34a35f0c77
lekova <lina.lekova@gmail.com>
2015-10-16 13:40:27 -0400

    Remove array of tile objects from controllers and get it from board factory.

--- app/scripts/controllers/fourByFiveController.js
+++ app/scripts/controllers/fourByFiveController.js
@@ -7,43 +7,21 @@
  * # FourByFiveCtrl is responsible for the page with 20 tiles
  * Controller of the gameApp
  */
-(function() {
+angular.module('gameApp').controller('FourByFiveCtrl', function(Boards, GameFactory) {
 
-	angular.module('gameApp').controller('FourByFiveCtrl', FourByFiveCtrl);
-	FourByFiveCtrl.$inject = ['$scope'];
+	var vm = this;
 
-	function FourByFiveCtrl($scope) {
+	vm.tiles_4x5 = Boards.tiles_4x5;
 
-		var vm = this;
-
-		vm.tiles_4x4 = [
-			{ id: 1, url: 'images/3x3/angularjs.png'},
-			{ id: 2, url: 'images/3x3/angularjs.png'},
-			{ id: 3, url: 'images/3x3/ember.png'},
-			{ id: 4, url: 'images/3x3/ember.png'},
-			{ id: 5, url: 'images/3x3/nodejs.png'},
-			{ id: 6, url: 'images/3x3/nodejs.png'},
-			{ id: 7, url: 'images/3x3/polymer.png'},
-			{ id: 8, url: 'images/3x3/polymer.png'},
-			{ id: 9, url: 'images/3x3/react.png'},
-			{ id: 10, url: 'images/3x3/react.png'},
-			{ id: 11, url: 'images/3x3/requirejs.png'},
-			{ id: 12, url: 'images/3x3/requirejs.png'},
-			{ id: 13, url: 'images/3x3/beerjs.png'},
-			{ id: 14, url: 'images/3x3/beerjs.png'},
-			{ id: 15, url: 'images/3x3/JS.png'},
-			{ id: 16, url: 'images/3x3/JS.png'},
-			{ id: 17, url: 'images/3x3/v8.png'},
-			{ id: 18, url: 'images/3x3/v8.png'},
-			{ id: 19, url: 'images/4x4/jasmine.png'},
-			{ id: 20, url: 'images/4x4/jasmine.png'},
-		];
-
-		vm.flip3D = function(id) {
-			var clickedFront = angular.element(document.querySelector('#front_' + id));
-			var clickedBack = angular.element(document.querySelector('#back_' + id));
-			clickedFront.addClass('front-image-clicked');
-			clickedBack.addClass('back-image-clicked');
-		}
+	vm.memory_tile_shuffle = function() {
+		console.log('memory shuffle clicked');
+		GameFactory.memory_tile_shuffle(vm.tiles_4x5);
 	};
-})();
+
+	vm.flip3D = function(id) {
+		var clickedFront = angular.element(document.querySelector('#front_' + id));
+		var clickedBack = angular.element(document.querySelector('#back_' + id));
+		clickedFront.addClass('front-image-clicked');
+		clickedBack.addClass('back-image-clicked');
+	};
+});
--- app/scripts/controllers/fourByFourController.js
+++ app/scripts/controllers/fourByFourController.js
@@ -7,39 +7,20 @@
  * # FourByFourCtrl is responsible for the page with 16 tiles
  * Controller of the gameApp
  */
-(function() {
+angular.module('gameApp').controller('FourByFourCtrl', function(Boards, GameFactory) {
+	var vm = this;
 
-	angular.module('gameApp').controller('FourByFourCtrl', FourByFourCtrl);
-	FourByFourCtrl.$inject = ['$scope'];
+	vm.tiles_4x4 = Boards.tiles_4x4;
 
-	function FourByFourCtrl($scope) {
-
-		var vm = this;
-
-		vm.tiles_4x4 = [
-			{ id: 1, url: 'images/3x3/angularjs.png'},
-			{ id: 2, url: 'images/3x3/angularjs.png'},
-			{ id: 3, url: 'images/3x3/ember.png'},
-			{ id: 4, url: 'images/3x3/ember.png'},
-			{ id: 5, url: 'images/3x3/nodejs.png'},
-			{ id: 6, url: 'images/3x3/nodejs.png'},
-			{ id: 7, url: 'images/3x3/polymer.png'},
-			{ id: 8, url: 'images/3x3/polymer.png'},
-			{ id: 9, url: 'images/3x3/react.png'},
-			{ id: 10, url: 'images/3x3/react.png'},
-			{ id: 11, url: 'images/3x3/requirejs.png'},
-			{ id: 12, url: 'images/3x3/requirejs.png'},
-			{ id: 13, url: 'images/3x3/beerjs.png'},
-			{ id: 14, url: 'images/3x3/beerjs.png'},
-			{ id: 15, url: 'images/3x3/JS.png'},
-			{ id: 16, url: 'images/3x3/JS.png'}
-		];
-
-		vm.flip3D = function(id) {
-			var clickedFront = angular.element(document.querySelector('#front_' + id));
-			var clickedBack = angular.element(document.querySelector('#back_' + id));
-			clickedFront.addClass('front-image-clicked');
-			clickedBack.addClass('back-image-clicked');
-		}
+	vm.memory_tile_shuffle = function() {
+		console.log('memory shuffle clicked');
+		GameFactory.memory_tile_shuffle(vm.tiles_4x4);
 	};
-})();
+
+	vm.flip3D = function(id) {
+		var clickedFront = angular.element(document.querySelector('#front_' + id));
+		var clickedBack = angular.element(document.querySelector('#back_' + id));
+		clickedFront.addClass('front-image-clicked');
+		clickedBack.addClass('back-image-clicked');
+	};
+});
